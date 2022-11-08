314079ebf6b5d2c58874c12fcbf377d674b3491b
Kaylan Thompson <kaylanthompson@icloud.com>
2022-11-08 12:29:24 -0600

    createUser and createUserById are functioning properly.

--- db/users.js
+++ db/users.js
@@ -1,41 +1,70 @@
-
-const client = require("./client");
+const client = require("./client")
 
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
-  try {
-    const { rows: [user] } = await client.query(`
+	try {
+		const {
+			rows: [user]
+		} = await client.query(
+			`
     INSERT INTO users(username, password ) 
-    VALUES($1, $2) 
-    ON CONFLICT (username) DO NOTHING 
-    RETURNING *;
-    `,[username, password]);
-
-    return user
-    
-  } catch (error) {
-    console.log(error)
-    throw error    
-  }
+    VALUES($1, $2)
+    ON CONFLICT (username) DO NOTHING
+    RETURNING *
+    `,
+			[username, password]
+		)
+		delete user.password
+		return user
+	} catch (error) {
+		console.log(error)
+		throw error
+	}
 }
 
 async function getUser({ username, password }) {
+	try {
+		const {
+			rows: [user]
+		} = await client.query(
+			`
+      SELECT * FROM users
+      WHERE password=$2
+    `,
+			[username, password]
+		)
 
+    delete user.password
+		return user
+	} catch (error) {
+		console.log(error)
+		throw error
+	}
 }
 
 async function getUserById(userId) {
-
+	try {
+		const {
+			rows: [user]
+		} = await client.query(`
+      SELECT * FROM users
+      WHERE id=${userId}
+    `)
+		delete user.password
+		return user
+	} catch (error) {
+		console.log(error)
+		throw error
+	}
 }
 
-async function getUserByUsername(userName) {
-
-}
+async function getUserByUsername(userName) {}
 
 module.exports = {
-  createUser,
-  getUser,
-  getUserById,
-  getUserByUsername,
+	createUser,
+	getUser,
+	getUserById,
+	getUserByUsername
 }
