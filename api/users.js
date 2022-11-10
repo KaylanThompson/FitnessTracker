91a71aba27cb10b6ceb5bd46f062363ca587d280
Kaylan Thompson <kaylanthompson@icloud.com>
2022-11-10 10:22:34 -0600

    nick fixed our server stuff so we can start routes

--- api/users.js
+++ api/users.js
@@ -1,12 +1,86 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const usersRouter = express.Router()
+const jwt = require("jsonwebtoken")
+const { JWT_SECRET } = process.env
+
+const { getUserByUsername, createUser } = require("../db")
 
 // POST /api/users/login
 
+usersRouter.post("/login", async (req, res, next) => {
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password"
+    })
+  }
+
+  try {
+    const user = await getUserByUsername(username)
+
+    if (user && user.password == password) {
+      const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, {
+        expiresIn: "7d"
+      })
+      const recoveredData = jwt.verify(token, JWT_SECRET)
+      res.send({ message: "you're logged in!", token: token })
+      return recoveredData
+    } else {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect"
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+
 // POST /api/users/register
+
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password } = req.body
+
+  try {
+    const _user = await getUserByUsername(username)
+
+    if (_user) {
+      next({
+        name: "UserExistsError",
+        message: "A user by that username already exists"
+      })
+    }
+
+    const user = await createUser({
+      username,
+      password
+    })
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1w"
+      }
+    )
+
+    res.send({
+      message: "thank you for signing up",
+      token
+    })
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
+})
 
 // GET /api/users/me
 
 // GET /api/users/:username/routines
 
-module.exports = router;
+module.exports = usersRouter
