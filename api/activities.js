43b0d2d255b666d98e95a2dcadcdc635200f12e8
irfan <ipekusic@yahoo.com>
2022-11-10 15:18:35 -0500

    made sure to save previous work

--- api/activities.js
+++ api/activities.js
@@ -1,23 +1,19 @@
 const express = require('express');
-const { getAllActivities } = require('../db');
+const { getAllActivities, } = require('../db');
 const activitiesRouter = express.Router();
 
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
 activitiesRouter.get('/', async (req, res ) =>{
-
-        const activities = await getAllActivities();
-        
-        
-        res.send(activities);
-
-
-        
-
+    const activities = await getAllActivities();
+    res.send(activities);
 });
 
 // POST /api/activities
+
+
+
 
 // PATCH /api/activities/:activityId
 
