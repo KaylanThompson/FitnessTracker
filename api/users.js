da30f5514f4947040c6ab378472034c24dce0f06
Kaylan Thompson <kaylanthompson@icloud.com>
2022-11-10 20:57:10 -0600

    all users routes are working

--- api/users.js
+++ api/users.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const usersRouter = express.Router()
 const jwt = require("jsonwebtoken")
-const { token } = require("morgan")
+// const { token } = require("morgan")
 const { JWT_SECRET } = process.env
 
-const { getUserByUsername, createUser, getUser, getPublicRoutinesByUser } = require("../db")
+const { getUserByUsername, createUser, getPublicRoutinesByUser, getUserById } = require("../db")
+
 const { requireUser } = require("./utils")
 
 // POST /api/users/login
@@ -90,24 +91,38 @@ usersRouter.post("/register", async (req, res, next) => {
 
 // GET /api/users/me
 
-usersRouter.get("/me", requireUser, async (req, res, next) => {
+usersRouter.get("/me", async (req, res, next) => {
 
- const user = await getUser({username, password})
-
-
-   res.send(user
-   )
-
-
-
- 
-
+  if (!req.user){
+    res.statusCode = 401
+    res.send({
+      error: "This is an error",
+      message: "You must be logged in to perform this action",
+      name: "Unauthorized Error"
     })
+  }
 
+  try {
+    await getUserById(req.user.id)
 
-
-  
+    res.send({
+      id: req.user.id,
+      username: req.user.username
+    })
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
+})
 
 // GET /api/users/:username/routines
+
+usersRouter.get("/:username/routines", async (req, res, next) => {
+  try {
+    const user = await getPublicRoutinesByUser(req.params)
+    res.send(user)
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
+})
 
 module.exports = usersRouter
