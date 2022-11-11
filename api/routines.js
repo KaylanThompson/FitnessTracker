9c52a39f6cd63a15ae4895d0e349d9ad767f567f
Kaylan Thompson <kaylanthompson@icloud.com>
2022-11-11 12:45:17 -0600

    accidently removed file from being tracked...adding it back in.

--- api/routines.js
+++ api/routines.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const routinesRouter = express.Router()
-const { getAllPublicRoutines, createRoutine, destroyRoutine, getRoutineById } = require("../db")
+const { getAllPublicRoutines, createRoutine, destroyRoutine, getRoutineById, getAllRoutinesByUser } = require("../db")
 const { requireUser } = require("./utils")
 
 // GET /api/routines
@@ -15,8 +15,10 @@ routinesRouter.get("/", async (req, res) => {
 routinesRouter.post("/", requireUser, async (req, res) => {
   const { creatorId, isPublic, name, goal } = req.body
 
+
   try {
       await createRoutine({ creatorId, isPublic, name, goal })
+
       
       res.send({
           creatorId: req.user.id,
@@ -34,11 +36,44 @@ routinesRouter.post("/", requireUser, async (req, res) => {
     
 // PATCH /api/routines/:routineId
 
+routinesRouter.patch("/:routineId", requireUser, async(req, res) => {
+  const idForRoutine = req.params.routineId
+  const { username } = req.body
+
+  try {
+
+    // const routine = await getAllRoutinesByUser({username})
+    const routine = await getRoutineById(idForRoutine)
+    if (routine.creatorId === req.user.id){
+      console.log(routine, "line 47")
+    res.send(routine)}
+    else {
+      res.statusCode = 403
+      // res.send({
+      //   message: "Unauthorized to edit this routine.",
+      //   name: "Can't Edit This Post",
+      //   error: "UnauthorizedToEdit"
+
+      // })
+    }
+    
+  } catch (error) {
+    console.log(error)
+    throw error
+    
+  }
+
+
+
+})
+
+
+
 // DELETE /api/routines/:routineId
 
 routinesRouter.delete("/:routineId", requireUser, async (req, res) => {
 const id = req.params.routineId
-  console.log("why are we still up")
+
     try {
            const routineToBeDeleted = await getRoutineById(id)
              if (routineToBeDeleted.creatorId === req.user.id) {
